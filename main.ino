51cb8e003fbde921ffc433a163cd3c1533d35a31
dhan <142086577+Dhans62@users.noreply.github.com>
2025-12-21 21:59:04 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,20 +1,14 @@
-#include <Arduino.h>
-#include <Update.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include "ble_ota.h"
 
-// UUID standar untuk Service dan Characteristic OTA
-#define SERVICE_UUID "fb1e4001-54aa-4b8b-9a35-0d605658d551"
-#define CHARACTERISTIC_UUID "fb1e4002-54aa-4b8b-9a35-0d605658d551"
-
-void setupBLEOTA() {
-    BLEDevice::init("ESP32_OTA_UPDATE");
-    BLEServer *pServer = BLEDevice::createServer();
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    // Di sini Anda bisa menambahkan logika Update.write() saat data diterima
-    pService->start();
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->start();
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Memulai Sistem dengan Core 3.0.7");
+    setupBLEOTA();
+    Serial.println("BLE OTA Siap Menunggu Koneksi...");
 }
+
+void loop() {
+    // Logika utama Anda di sini
+    delay(1000);
+}
+
